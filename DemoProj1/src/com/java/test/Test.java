1ac960a5843aae4067dfb5bdff607080f770e198
TL <TL@gmail.com>
2020-04-14 22:20:15 +0530

    Dev1 modified Test.java

--- DemoProj1/src/com/java/test/Test.java
+++ DemoProj1/src/com/java/test/Test.java
@@ -1,5 +1,14 @@
 package com.java.test;
 
+import com.java.beans.Utility;
+
 public class Test {
 
+	public static void main(String []args) {
+		
+		Utility u = new Utility();
+		
+		System.out.println(u.getSysDate());
+	}
+	
 }
