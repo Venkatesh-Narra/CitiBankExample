abaf0c94ddfc7f8a603d1bcf23b463eae86344dd
Dev2 <Dev2@gmail.com>
2020-04-16 15:33:19 +0530

    m3 method replaced from stash

--- DemoProj1/src/com/java/beans/Utility.java
+++ DemoProj1/src/com/java/beans/Utility.java
@@ -18,8 +18,14 @@ public class Utility {
 		System.out.println("m2 method");
 	}
 	
+
 	public void m4() {
 		System.out.println("m4 method");
+	}
+
+	public void m3() {
+		
+		System.out.println("m3 method");
 	}
 
 }
